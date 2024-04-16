55f8291aca34cec324539dae8461f70a2d143491
deeprathod97 <deeprathod9797@gmail.com>
2024-04-16 12:47:51 +0530

    js-2

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("hey this is tutorial 2 of Js")
+
+let a = 5;
+a = a +1
+let b = 6;
+let c ="deep";
+let _a = "Kunal";
+// var 55a = "jay"; // Not Allowed
+
+console.log(a + b + c + 9)
+console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66;
+    console.log(a)
+}
+// const a1 = 6;
+// a1 = a1 + 1:
+// Not allowed because a1 is contant 
+
+
+let x = "Deep Bhai";
+let y = 19;
+let z = 3.55;
+const p = true
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "Deep ",
+    "Job code": 5600,
+    "is_handssom": true
+}
+
+console.log(o)
+o.salary = "100crores"
+console.log(o)
+o.salary = "700croores";
+console.log(o)
